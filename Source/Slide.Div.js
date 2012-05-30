280f1bf27fa445a30384de470737ed32ac757658
daKmoR <thomas.allmer@webteam.at>
2012-05-30 18:34:27 +0200

    [TASK] add basic fancyness to Slide.Div to be used as description of a slide

--- Source/Slide.Div.js
+++ Source/Slide.Div.js
@@ -14,7 +14,37 @@ provides: [Slide.Div]
 
 Slide.Div = new Class({
 
-	Extends: Slide.Element
+	Extends: Slide.Element,
+
+	show: function() {
+		this.parent();
+		this.element.tween('bottom', [-this.slide.getSize().height, 0]);
+//		var triggers = this.element.getTriggers();
+//		console.log(triggers);
+	},
+
+//	show: function() {
+//		this.element.set('style', this.savedStyle);
+//		this.element.setStyle('display', 'block');
+//	},
+
+	hide: function() {
+		//this.parent();
+		//this.element.setStyle('zIndex', 100);
+		this.element.tween('bottom', [0, -this.slide.getSize().height]);
+	}
+
+//	hide: function() {
+//		this.element.setStyle('zIndex', 100);
+//		this.element.fade(0).get('tween').chain(
+//			function() {
+//				this.element.set('style', this.savedStyle);
+//				this.callChain();
+//			}.bind(this)
+//		);
+//
+//		return this;
+//	},
 
 });
 
